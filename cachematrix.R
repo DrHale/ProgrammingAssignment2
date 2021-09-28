bea01ae09eec80fa1d67d4cbb7cf313c068d6ca8
DrHale <drjphale@gmail.com>
2021-09-28 13:27:20 -0400

    Add functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a cacheMatrix object from a numeric matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes and caches inverse of cacheMatrix matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
+
+# Test functions
+
+n2 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2,ncol = 2)
+testm = makeCacheMatrix(n2)
+cacheSolve(testm)
+cacheSolve(testm)
+
